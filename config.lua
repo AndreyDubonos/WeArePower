c05e7b914e5b1ae28eba1348b5d7e8360315dafb
AndreyDubonos <andrey.d@it-dimension.com>
2017-09-12 08:09:40 +0300

    created gameScene

--- config.lua
+++ config.lua
@@ -2,21 +2,21 @@
 -- For more information on config.lua see the Project Configuration Guide at:
 -- https://docs.coronalabs.com/guide/basics/configSettings
 --
+local aspectRatio = display.pixelHeight / display.pixelWidth
 
 application =
 {
 	content =
 	{
-		width = 320,
-		height = 480, 
-		scale = "letterbox",
+		width =  aspectRatio > 1.5 and 750 or 768,
+		height = aspectRatio > 1.5 and 1334 or 1024, 
+		scale = "letterBox",
 		fps = 60,
 		
 		--[[
 		imageSuffix =
 		{
 			    ["@2x"] = 2,
-			    ["@4x"] = 4,
 		},
 		--]]
 	},
--- main.lua
+++ main.lua
@@ -1,7 +1,4 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+display.setStatusBar( display.HiddenStatusBar )
 
--- Your code here
+local composer = require "composer"
+composer.gotoScene( "gameScene" )
